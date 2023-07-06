74ac49a30b6e1e078afe4566cb55b8e44e472eba
GabiBarbera <gabriel.barberaa@gmail.com>
2023-07-05 23:19:13 -0300

    Se agregaron las cartas por js

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let container = document.getElementById("allCards")
+
+function createLetters(object) {
+    return ` <div class="card col-md-2 float-md-end mb-3 ms-md-3 bg-success shadow-lg p-3 mb-5 bg-body-success rounded">
+    <img src="${object.image}" class="card-img w-100 object-fit-cover" alt="cinema">
+    <div class="card-body d-flex flex-column justify-content-evenly">
+    <h5 class="card-title align-self-center">${object.name}</h5>
+    <p class="card-text">${object.description}</p>
+    <div class="d-flex justify-content-between align-items-center">
+    <h6>US$${object.price}</h6>
+    <a href="./assets/pages/details.html" class="btn btn-primary">Details</a>
+        </div>
+    </div>
+</div>`
+}
+
+
+function showCards() {
+    for (let info of data.events) {
+        container.innerHTML += createLetters(info)
+    }
+}
+showCards()
